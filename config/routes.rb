b5294d7d7ac89d6b89802a63edbfd86f328ab2b8
benoit moret <benoit.moret@gmail.com>
2023-11-20 17:59:03 +0100

    exo completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/flats", to: "flats#index"
 
+  get "appartements", to: "appartements#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
